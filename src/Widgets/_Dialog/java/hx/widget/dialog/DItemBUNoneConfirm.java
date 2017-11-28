6ce615191574038046cc447c54c09bacaf1d5c3a
RoseHongXin <rose_hong@foxmail.com>
2017-11-28 18:15:48 +0800

    1.全景图片相关逻辑添加. 2.搜索页面相关逻辑修改.

--- src/Widgets/_Dialog/java/hx/widget/dialog/DItemBUNoneConfirm.java
+++ src/Widgets/_Dialog/java/hx/widget/dialog/DItemBUNoneConfirm.java
@@ -8,6 +8,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -85,8 +86,8 @@ public class DItemBUNoneConfirm extends DialogFragment{
         return texts(list);
     }
 
-    public DItemBUNoneConfirm show(FragmentManager fraManager){
-        show(fraManager, "DItemBUNoneConfirm");
+    public DItemBUNoneConfirm show(){
+        show(((AppCompatActivity)mAct).getSupportFragmentManager(), "DItemBUNoneConfirm");
         return this;
     }
 
--- src/Widgets/_Dialog/java/hx/widget/dialog/DialogHelper.java
+++ src/Widgets/_Dialog/java/hx/widget/dialog/DialogHelper.java
@@ -19,12 +19,18 @@ public class DialogHelper {
         config(dialog, gravity, 0, 0, 0, 0);
     }
 
+    public static void padding(Dialog dialog, int gravity){
+        config(dialog, gravity, 24, 24, 24, 24);
+    }
     public static void padding(Dialog dialog){
         config(dialog, Gravity.CENTER, 24, 24, 24, 24);
     }
 
     public static void padding(Dialog dialog, int paddingHorizontal, int paddingVertical){
         config(dialog, Gravity.CENTER, paddingHorizontal, paddingVertical, paddingHorizontal, paddingVertical);
+    }
+    public static void padding(Dialog dialog, int gravity, int paddingHorizontal, int paddingVertical){
+        config(dialog, gravity, paddingHorizontal, paddingVertical, paddingHorizontal, paddingVertical);
     }
 
     private static void config(Dialog dialog, int gravity, int paddingLeft, int paddingTop, int paddingRight, int paddingBottom){
